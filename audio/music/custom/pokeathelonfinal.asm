4d7828070deb50e7458fcd1d8b4caeb34bf41fd5
U-Myles-PC\Myles <froggestspirit@gmail.com>
2014-01-30 12:08:52 -0500

    more music and pokethelon fix

--- audio/music/custom/pokeathelonfinal.asm
+++ audio/music/custom/pokeathelonfinal.asm
@@ -7,443 +7,63 @@ Music_PokeathelonFinal:
 Music_PokeathelonFinal_Ch1:
 	tempo $84
 	volume $77
-	notetype $C, $C3
+	notetype $2, $C3
 	vibrato 8, $24
 	dutycycle 2
+	
 	octave 4
-	unknownmusic0xe0 0, $58
 	note D#, 1
-	octave 3
-	unknownmusic0xe0 0, $52
-	note A_, 1
-	unknownmusic0xe0 0, $68
-	note D#, 1
-	octave 1
-	unknownmusic0xe0 0, $71
 	note D_, 1
-	unknownmusic0xe0 0, $71
-	note D_, 1
-	octave 4
-	unknownmusic0xe0 0, $58
-	note D#, 1
-	octave 3
-	unknownmusic0xe0 0, $52
-	note A_, 1
-	unknownmusic0xe0 0, $68
-	note D#, 1
-	intensity $C2
-	dutycycle 1
-	note F_, 1
-	note F#, 1
-	note G#, 1
-	octave 4
+	note C#, 2
 	note C_, 1
-	note F_, 1
-	note D#, 1
-	note C#, 1
-	note C_, 1
-	note C#, 8
-Music_PokeathelonFinal_Loop1:
-	note __, 3
-	notetype $6, $C2
-	note __, 1
-	note C_, 1
-	note C#, 4
-	notetype $C, $C2
-	note D#, 13
-	notetype $6, $C2
-	note __, 1
-	note D_, 1
-	note D#, 4
-	notetype $C, $C2
-	note C#, 13
-	notetype $6, $C2
-	note __, 1
-	note C_, 1
-	note C#, 4
-	notetype $C, $C2
-	octave 3
-	note A#, 6
-	intensity $B2
-	dutycycle 2
-	octave 4
-	note A#, 1
-	note F_, 1
-	note A#, 1
-	octave 5
-	note C#, 1
-	note F_, 1
-	note D#, 1
-	note C#, 1
-	note C_, 1
-	octave 4
-	note A#, 1
-	note A_, 1
-	note A#, 1
-	octave 5
-	note C_, 1
-	intensity $C2
-	dutycycle 1
-	octave 4
-	note C#, 11
-	notetype $6, $C2
-	note __, 1
-	note C_, 1
-	note C#, 4
-	notetype $C, $C2
-	note D#, 13
-	notetype $6, $C2
-	note __, 1
-	note D_, 1
-	note D#, 4
-	notetype $C, $C2
-	note F_, 13
-	notetype $6, $C2
-	note __, 1
-	note E_, 1
-	note F_, 4
-	notetype $C, $C2
-	note F#, 10
 	octave 3
 	note B_, 1
-	octave 4
-	note F_, 1
-	note G#, 6
-	intensity $C7
-	dutycycle 0
-	octave 3
-	note A#, 10
-	note F_, 2
-	note A#, 2
-	note G#, 4
-	note G_, 4
-	note D#, 8
-	note D#, 1
-	note F_, 1
-	note F#, 12
-	note G#, 2
-	note F_, 6
-	intensity $B2
-	dutycycle 2
-	octave 4
-	note A#, 1
-	note F_, 1
-	note A#, 1
-	octave 5
-	note C#, 1
-	note F_, 1
-	note D#, 1
-	note C#, 1
-	note C_, 1
-	octave 4
-	note A#, 1
+	
 	note A_, 1
-	note A#, 1
-	octave 5
-	note C_, 1
-	intensity $C7
-	dutycycle 0
-	octave 3
-	note A#, 10
-	note D#, 2
-	note A#, 2
-	note G#, 8
-	note G_, 6
-	note D#, 2
-	note D#, 1
-	note F_, 1
-	note F#, 12
-	note G#, 2
-	note A#, 8
-	note A_, 1
-	note A#, 1
-	octave 4
-	note C_, 8
-	intensity $C7
-	dutycycle 1
-	note A#, 10
-	note F_, 2
-	note A#, 2
-	note G#, 4
-	note G_, 4
-	note D#, 8
-	note D#, 1
-	note F_, 1
-	note F#, 12
-	note G#, 2
-	note F_, 10
-	octave 5
-	note D#, 1
-	note F_, 1
-	note D#, 1
-	note C#, 1
-	note C_, 1
-	octave 4
-	note A#, 1
 	note G#, 1
-	note F_, 1
-	note A#, 10
-	note D#, 2
-	note A#, 2
-	note G#, 8
-	note G_, 6
-	note D#, 1
-	note __, 1
-	note D#, 1
-	note F_, 1
-	note F#, 12
-	note G#, 2
-	note A#, 8
-	intensity $C2
-	note A_, 1
-	note A#, 1
-	octave 5
-	note C_, 1
-	note C_, 1
-	octave 4
-	note A_, 1
-	note F_, 1
-	note C_, 1
-	note F_, 1
-	dutycycle 2
-	octave 3
-	note G_, 1
-	note A_, 1
-	note B_, 2
-	dutycycle 1
-	note G_, 1
-	note D_, 1
-	octave 2
-	note B_, 1
-	octave 3
-	note D_, 1
-	dutycycle 2
 	note G_, 2
-	dutycycle 1
-	note D_, 1
-	octave 2
-	note B_, 1
-	note G_, 1
-	note B_, 1
-	dutycycle 2
-	octave 3
-	note B_, 2
-	dutycycle 1
-	note G_, 1
-	note D_, 1
-	dutycycle 2
-	note A_, 2
-	dutycycle 1
-	note E_, 1
-	note C#, 1
-	octave 2
-	note A_, 1
-	octave 3
-	note C#, 1
-	dutycycle 2
-	note E_, 2
-	dutycycle 1
-	note C#, 1
-	octave 2
-	note A_, 1
-	note E_, 1
-	note A_, 1
-	dutycycle 2
-	octave 3
-	note E_, 2
-	dutycycle 1
-	note C#, 1
-	octave 2
-	note A_, 1
-	dutycycle 2
-	octave 3
-	note D_, 2
-	dutycycle 1
-	octave 2
-	note B_, 1
 	note F#, 1
+	note F_, 1
+
+	note D#, 1
 	note D_, 1
-	note F#, 1
-	dutycycle 2
-	octave 3
-	note A_, 2
-	dutycycle 1
-	note F#, 1
-	note D_, 1
-	octave 2
-	note B_, 1
-	octave 3
-	note D_, 1
-	dutycycle 2
-	note B_, 2
-	dutycycle 1
-	note F#, 1
-	note D_, 1
-	dutycycle 2
 	note C#, 2
-	dutycycle 1
+	note C_, 1
 	octave 2
-	note A_, 1
-	note F#, 1
-	octave 3
-	note F#, 1
+	note B_, 1
+
+	octave 1
+	note D_, 1
 	note C#, 1
-	octave 2
-	note A_, 1
-	octave 3
+	note C_, 4
+
+	note D_, 1
 	note C#, 1
-	note A_, 1
-	note F#, 1
-	note C#, 1
-	note F#, 1
+	note C_, 4
+	
 	octave 4
-	note C#, 1
-	octave 3
-	note A_, 1
-	dutycycle 2
-	note C#, 1
+	note D#, 1
 	note D_, 1
-	note E_, 2
-	dutycycle 1
-	note E_, 1
-	note C_, 1
-	octave 2
-	note G_, 1
-	octave 3
-	note C_, 1
-	dutycycle 2
-	note E_, 2
-	dutycycle 1
-	note E_, 1
-	note C_, 1
-	octave 2
-	note G_, 1
-	octave 3
-	note C_, 1
-	dutycycle 2
-	note F#, 2
-	dutycycle 1
-	note C_, 1
-	octave 2
-	note G_, 1
-	dutycycle 2
-	octave 3
-	note D_, 2
-	dutycycle 1
-	note D_, 1
-	octave 2
-	note B_, 1
-	note F#, 1
-	note B_, 1
-	dutycycle 2
-	octave 3
 	note C#, 2
-	dutycycle 1
-	octave 2
+	note C_, 1
+	octave 3
 	note B_, 1
-	note F#, 1
-	note D_, 1
-	note F#, 1
-	dutycycle 2
-	note B_, 2
-	dutycycle 1
-	note F#, 1
-	note D_, 1
-	dutycycle 2
-	octave 3
-	note C_, 2
-	dutycycle 1
-	note C_, 1
-	octave 2
+	
 	note A_, 1
-	note E_, 1
-	note C_, 1
-	dutycycle 2
-	note A_, 2
-	dutycycle 1
-	note E_, 1
-	note C_, 1
-	note E_, 1
-	note A_, 1
-	dutycycle 2
-	octave 3
-	note C_, 2
-	dutycycle 1
-	octave 2
-	note A_, 1
-	note E_, 1
-	dutycycle 2
-	octave 3
-	note D_, 2
-	dutycycle 1
-	octave 2
-	note A_, 1
-	note F#, 1
-	note D_, 1
-	note F#, 1
-	dutycycle 2
-	octave 3
-	note E_, 1
-	note D_, 1
-	intensity $B0
-	dutycycle 1
-	octave 4
-	note C#, 1
-	octave 3
-	note E_, 1
-	note F#, 1
 	note G#, 1
-	note A#, 1
+	note G_, 2
+	note F#, 1
+	note F_, 1
+
+	note D#, 1
+	note D_, 1
+	note C#, 2
+	note C_, 1
+	octave 2
 	note B_, 1
-	octave 4
-	note C#, 1
-	note D#, 1
-	note E_, 6
-	note C#, 6
-	note E_, 4
-	note F#, 6
-	octave 3
-	note B_, 6
-	octave 4
-	notetype $6, $B0
-	note F#, 7
-	unknownmusic0xe0 0, $4A
-	note F#, 1
-	notetype $C, $B0
-	note A_, 6
-	note G#, 6
-	note F#, 4
-	note E_, 15
-	unknownmusic0xe0 0, $49
-	note F#, 1
-	note G#, 6
-	note F#, 6
-	note B_, 4
-	note G#, 6
-	note F#, 6
-	note E_, 4
-	note D#, 16
-	note C#, 6
-	intensity $C2
-	note D#, 1
-	note C#, 1
-	note C_, 1
-	octave 3
-	note F_, 1
-	note A_, 1
-	octave 4
-	note C_, 1
-	note F_, 1
-	note D#, 1
-	note C#, 1
-	note C_, 1
-	note C#, 8
-	loopchannel 0, Music_PokeathelonFinal_Loop1
-Music_PokeathelonFinal_Ch2:
+	
 	notetype $C, $C2
 	dutycycle 2
-	vibrato 8, $24
-	note __, 8
 	octave 4
 	note F_, 1
 	note F#, 1
@@ -459,133 +79,166 @@ Music_PokeathelonFinal_Ch2:
 	octave 3
 	note F_, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note F_, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
-Music_PokeathelonFinal_Loop2:
+Music_PokeathelonFinal_Loop1:
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note F_, 2
 	octave 3
 	note F_, 2
 	note G_, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note G_, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note G_, 2
 	octave 3
 	note G_, 2
 	note F#, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note F#, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note F#, 2
 	octave 3
 	note F#, 2
 	note C#, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note F_, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note F_, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note F_, 2
 	octave 3
 	note G_, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note D#, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note D#, 2
 	octave 3
 	note G_, 2
 	note A#, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note G_, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note G_, 2
 	octave 3
 	note G_, 2
 	note G#, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note G#, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note G#, 2
 	octave 3
 	note G#, 2
 	note A#, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	note A#, 1
 	octave 2
+	intensity $C3
 	note A#, 2
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	note A#, 1
@@ -596,23 +249,23 @@ Music_PokeathelonFinal_Loop2:
 	intensity $C3
 	octave 4
 	note C#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
@@ -621,29 +274,29 @@ Music_PokeathelonFinal_Loop2:
 	note C_, 1
 	note C#, 2
 	note D#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
-	octave 2
-	note A#, 1
-	note A#, 1
-	note A#, 1
-	intensity $A2
-	octave 4
-	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
+	octave 2
+	note A#, 1
+	note A#, 1
+	note A#, 1
+	intensity $91
+	octave 4
+	note A#, 1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
@@ -652,29 +305,29 @@ Music_PokeathelonFinal_Loop2:
 	note D_, 1
 	note D#, 2
 	note C#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
-	octave 2
-	note A#, 1
-	note A#, 1
-	note A#, 1
-	intensity $A2
-	octave 4
-	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
+	octave 2
+	note A#, 1
+	note A#, 1
+	note A#, 1
+	intensity $91
+	octave 4
+	note A#, 1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
@@ -682,38 +335,38 @@ Music_PokeathelonFinal_Loop2:
 	octave 3
 	note C_, 1
 	note C#, 2
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
-	octave 2
-	note A#, 1
-	note A#, 1
-	note A#, 1
-	intensity $A2
-	octave 4
-	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
+	octave 2
+	note A#, 1
+	note A#, 1
+	note A#, 1
+	intensity $91
+	octave 4
+	note A#, 1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
@@ -721,23 +374,23 @@ Music_PokeathelonFinal_Loop2:
 	intensity $C3
 	octave 3
 	note C_, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
@@ -746,58 +399,58 @@ Music_PokeathelonFinal_Loop2:
 	note C_, 1
 	note C#, 2
 	note D#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
-	octave 2
-	note A#, 1
-	note A#, 1
-	note A#, 1
-	intensity $A2
-	octave 4
-	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
+	octave 2
+	note A#, 1
+	note A#, 1
+	note A#, 1
+	intensity $91
+	octave 4
+	note A#, 1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	intensity $C3
 	note F#, 1
 	note G_, 2
-	intensity $A1
+	intensity $81
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
@@ -806,37 +459,37 @@ Music_PokeathelonFinal_Loop2:
 	note E_, 1
 	note F_, 2
 	note F#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
-	octave 2
-	note A#, 1
-	note A#, 1
-	note A#, 1
-	intensity $A2
-	octave 4
-	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
 	note A#, 1
-	intensity $A2
+	intensity $91
 	octave 4
 	note A#, 1
-	intensity $A1
+	intensity $81
+	octave 2
+	note A#, 1
+	note A#, 1
+	note A#, 1
+	intensity $91
+	octave 4
+	note A#, 1
+	intensity $81
 	octave 2
 	note A#, 1
 	note A#, 1
@@ -933,7 +586,10 @@ Music_PokeathelonFinal_Loop2:
 	intensity $C7
 	dutycycle 1
 	octave 4
+	notetype $6, $C7
 	note A#, 1
+	note B_, 1
+	notetype $C, $C7
 	octave 5
 	note C_, 6
 	note C_, 1
@@ -1310,15 +966,435 @@ Music_PokeathelonFinal_Loop2:
 	note F_, 2
 	dutycycle 0
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
 	dutycycle 1
 	octave 2
+	intensity $C3
 	note F_, 2
 	dutycycle 0
 	octave 1
+	intensity $81
 	note A#, 1
 	note A#, 1
+	loopchannel 0, Music_PokeathelonFinal_Loop1
+Music_PokeathelonFinal_Ch2:
+	notetype $C, $C2
+	vibrato 8, $24
+	dutycycle 1
+	octave 3
+	note __, 8
+	note F_, 1
+	note F#, 1
+	note G#, 1
+	octave 4
+	note C_, 1
+	note F_, 1
+	note D#, 1
+	note C#, 1
+	note C_, 1
+	note C#, 8
+Music_PokeathelonFinal_Loop2:
+	notetype $4, $C2
+	note __, 11
+	note C_, 1
+	notetype $C, $C2
+	note C#, 2
+	note D#, 10
+	notetype $4, $C2
+	note __, 11
+	note D_, 1
+	notetype $C, $C2
+	note D#, 2
+	note C#, 10
+	notetype $4, $C2
+	note __, 11
+	note C_, 1
+	notetype $C, $C2
+	note C#, 2
+	octave 3
+	note A#, 6
+	intensity $B1
+	dutycycle 2
+	octave 4
+	note A#, 1
+	note F_, 1
+	note A#, 1
+	octave 5
+	note C#, 1
+	note F_, 1
+	note D#, 1
+	note C#, 1
+	note C_, 1
+	octave 4
+	note A#, 1
+	note A_, 1
+	note A#, 1
+	octave 5
+	note C_, 1
+	intensity $C2
+	dutycycle 1
+	octave 4
+	note C#, 8
+	notetype $4, $C2
+	note __, 11
+	note C_, 1
+	notetype $C, $C2
+	note C#, 2
+	note D#, 10
+	notetype $4, $C2
+	note __, 11
+	note D_, 1
+	notetype $C, $C2
+	note D#, 2
+	note F_, 10
+	notetype $4, $C2
+	note __, 11
+	note E_, 1
+	notetype $C, $C2
+	note F_, 2
+	note F#, 10
+	notetype $4, $C2
+	octave 3
+	note B_, 1
+	octave 4
+	note C_, 1
+	note D_, 1
+	note E_, 1
+	note F_, 1
+	note G_, 1
+	notetype $C, $C2
+	note G#, 6
+	intensity $C8
+	dutycycle 0
+	octave 3
+	note A#, 10
+	note F_, 2
+	note A#, 2
+	note G#, 4
+	note G_, 4
+	note D#, 8
+	note D#, 1
+	note F_, 1
+	note F#, 12
+	note G#, 2
+	note F_, 6
+	intensity $B1
+	dutycycle 2
+	octave 4
+	note A#, 1
+	note F_, 1
+	note A#, 1
+	octave 5
+	note C#, 1
+	note F_, 1
+	note D#, 1
+	note C#, 1
+	note C_, 1
+	octave 4
+	note A#, 1
+	note A_, 1
+	note A#, 1
+	octave 5
+	note C_, 1
+	intensity $C8
+	dutycycle 0
+	octave 3
+	note A#, 10
+	note D#, 2
+	note A#, 2
+	note G#, 8
+	note G_, 6
+	note D#, 2
+	note D#, 1
+	note F_, 1
+	note F#, 12
+	note G#, 2
+	note A#, 8
+	note A_, 1
+	note A#, 1
+	octave 4
+	note C_, 8
+	dutycycle 1
+	note A#, 10
+	note F_, 2
+	note A#, 2
+	note G#, 4
+	note G_, 4
+	note D#, 8
+	note D#, 1
+	note F_, 1
+	note F#, 12
+	note G#, 2
+	note F_, 10
+	octave 5
+	note D#, 1
+	note F_, 1
+	note D#, 1
+	note C#, 1
+	note C_, 1
+	octave 4
+	note A#, 1
+	note G#, 1
+	note F_, 1
+	note A#, 10
+	note D#, 2
+	note A#, 2
+	note G#, 8
+	note G_, 6
+	note D#, 1
+	note __, 1
+	note D#, 1
+	note F_, 1
+	note F#, 12
+	note G#, 2
+	note A#, 8
+	intensity $C2
+	note A_, 1
+	note A#, 1
+	octave 5
+	note C_, 1
+	note C_, 1
+	octave 4
+	note A_, 1
+	note F_, 1
+	note C_, 1
+	note F_, 1
+	dutycycle 2
+	octave 3
+	note G_, 1
+	note A_, 1
+	note B_, 2
+	dutycycle 1
+	note G_, 1
+	note D_, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note D_, 1
+	dutycycle 2
+	note G_, 2
+	dutycycle 1
+	note D_, 1
+	octave 2
+	note B_, 1
+	note G_, 1
+	note B_, 1
+	dutycycle 2
+	octave 3
+	note B_, 2
+	dutycycle 1
+	note G_, 1
+	note D_, 1
+	dutycycle 2
+	note A_, 2
+	dutycycle 1
+	note E_, 1
+	note C#, 1
+	octave 2
+	note A_, 1
+	octave 3
+	note C#, 1
+	dutycycle 2
+	note E_, 2
+	dutycycle 1
+	note C#, 1
+	octave 2
+	note A_, 1
+	note E_, 1
+	note A_, 1
+	dutycycle 2
+	octave 3
+	note E_, 2
+	dutycycle 1
+	note C#, 1
+	octave 2
+	note A_, 1
+	dutycycle 2
+	octave 3
+	note D_, 2
+	dutycycle 1
+	octave 2
+	note B_, 1
+	note F#, 1
+	note D_, 1
+	note F#, 1
+	dutycycle 2
+	octave 3
+	note A_, 2
+	dutycycle 1
+	note F#, 1
+	note D_, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note D_, 1
+	dutycycle 2
+	note B_, 2
+	dutycycle 1
+	note F#, 1
+	note D_, 1
+	dutycycle 2
+	note C#, 2
+	dutycycle 1
+	octave 2
+	note A_, 1
+	note F#, 1
+	octave 3
+	note F#, 1
+	note C#, 1
+	octave 2
+	note A_, 1
+	octave 3
+	note C#, 1
+	note A_, 1
+	note F#, 1
+	note C#, 1
+	note F#, 1
+	octave 4
+	note C#, 1
+	octave 3
+	note A_, 1
+	dutycycle 2
+	note C#, 1
+	note D_, 1
+	note E_, 2
+	dutycycle 1
+	note E_, 1
+	note C_, 1
+	octave 2
+	note G_, 1
+	octave 3
+	note C_, 1
+	dutycycle 2
+	note E_, 2
+	dutycycle 1
+	note E_, 1
+	note C_, 1
+	octave 2
+	note G_, 1
+	octave 3
+	note C_, 1
+	dutycycle 2
+	note F#, 2
+	dutycycle 1
+	note C_, 1
+	octave 2
+	note G_, 1
+	dutycycle 2
+	octave 3
+	note D_, 2
+	dutycycle 1
+	note D_, 1
+	octave 2
+	note B_, 1
+	note F#, 1
+	note B_, 1
+	dutycycle 2
+	octave 3
+	note C#, 2
+	dutycycle 1
+	octave 2
+	note B_, 1
+	note F#, 1
+	note D_, 1
+	note F#, 1
+	dutycycle 2
+	note B_, 2
+	dutycycle 1
+	note F#, 1
+	note D_, 1
+	dutycycle 2
+	octave 3
+	note C_, 2
+	dutycycle 1
+	note C_, 1
+	octave 2
+	note A_, 1
+	note E_, 1
+	note C_, 1
+	dutycycle 2
+	note A_, 2
+	dutycycle 1
+	note E_, 1
+	note C_, 1
+	note E_, 1
+	note A_, 1
+	dutycycle 2
+	octave 3
+	note C_, 2
+	dutycycle 1
+	octave 2
+	note A_, 1
+	note E_, 1
+	dutycycle 2
+	octave 3
+	note D_, 2
+	dutycycle 1
+	octave 2
+	note A_, 1
+	note F#, 1
+	note D_, 1
+	note F#, 1
+	dutycycle 2
+	octave 3
+	note E_, 1
+	note D_, 1
+	intensity $B0
+	dutycycle 1
+	octave 4
+	note C#, 1
+	octave 3
+	note E_, 1
+	note F#, 1
+	note G#, 1
+	note A#, 1
+	note B_, 1
+	octave 4
+	note C#, 1
+	note D#, 1
+	note E_, 6
+	note C#, 6
+	note E_, 4
+	note F#, 6
+	octave 3
+	note B_, 6
+	octave 4
+	notetype $6, $B0
+	note F#, 8
+	note G#, 1
+	note A_, 11
+	notetype $C, $B0
+	note G#, 6
+	note F#, 4
+	note E_, 15
+	notetype $6, $B0
+	note F#, 1
+	note G_, 1
+	notetype $C, $B0
+	note G#, 6
+	note F#, 6
+	note B_, 4
+	note G#, 6
+	note F#, 6
+	note E_, 4
+	note D#, 16
+	note C#, 6
+	intensity $C2
+	note D#, 1
+	note C#, 1
+	note C_, 1
+	octave 3
+	note F_, 1
+	note A_, 1
+	octave 4
+	note C_, 1
+	note F_, 1
+	note D#, 1
+	note C#, 1
+	note C_, 1
+	note C#, 8
 	loopchannel 0, Music_PokeathelonFinal_Loop2
 Music_PokeathelonFinal_Ch3:
 	notetype $C, $14
