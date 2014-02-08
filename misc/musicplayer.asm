2ccc2446cc0b14bbe6d78f2b887e19aec575d64a
U-Myles-PC\Myles <froggestspirit@gmail.com>
2014-02-08 10:56:46 -0500

    Revisions

--- misc/musicplayer.asm
+++ misc/musicplayer.asm
@@ -3,7 +3,7 @@ INCLUDE "includes.asm"
 
 SECTION "Music_Player", ROMX, BANK[MUSIC_PLAYER]
 
-NUMSONGS EQU 167
+NUMSONGS EQU 168
 
 MusicTestGFX:
 INCBIN "gfx/misc/music_test.2bpp"
@@ -96,8 +96,10 @@ MusicPlayer:
 .down
     ld a, [wSongSelection]
     sub a, 10
+	jr z, .zerofix
     cp a, NUMSONGS
     jr c, .redraw
+.zerofix
     ld a, NUMSONGS-1
     jr .redraw
 .up
@@ -114,10 +116,7 @@ MusicPlayer:
 	ld bc, 95
 	call ByteFill
 	hlcoord 0, 8
-	ld bc, 40
-	call ByteFill
-	hlcoord 0, 11
-	ld bc, 40
+	ld bc, 90
 	call ByteFill
 	hlcoord 5, 2
 	ld de, wSongSelection
@@ -159,74 +158,64 @@ MusicPlayer:
     ret
 
 DrawChData:
-    printbit Channel1Flags,  0, 0, 15, "1", "-" ; enabled
-    printbit Channel1Flags,  4, 1, 15, "N", "n" ; noise sampling
-    printbit Channel1Flags2, 0, 2, 15, "V", "v" ; vibrato
-    printbit Channel1Flags2, 2, 3, 15, "D", "d" ; dutycycle
-    printbit Channel1Flags3, 0, 4, 15, "U", "D" ; vibrato up/down
     ld a, [Channel1Pitch]
     ld hl, NoteNames
     call GetNthString
     push hl
     pop de
-    hlcoord 1, 16
+    hlcoord 0, 14
     call PlaceString
     ld a, [Channel1Octave]
     add $f6
-    hlcoord 3, 16
+    hlcoord 2, 14
     ld [hli], a
     
-    hlcoord 0, 14
-	ld de, Channel1Tempo
-	ld bc, $0103
-	call PrintNum
+    ;hlcoord 0, 1
+	;ld de, Channel1Tempo ;Maybe wait till we can get BPM
+	;ld bc, $0103
+	;call PrintNum
 	
 	
 ; CHANNEL 2
-    printbit Channel1Flags+$32,  0, 6+0, 15, "1", "-" ; enabled
-    printbit Channel1Flags+$32,  4, 6+1, 15, "N", "n" ; noise sampling
-    printbit Channel1Flags2+$32, 0, 6+2, 15, "V", "v" ; vibrato
-    printbit Channel1Flags2+$32, 2, 6+3, 15, "D", "d" ; dutycycle
-    printbit Channel1Flags3+$32, 0, 6+4, 15, "U", "D" ; vibrato up/down
     ld a, [$c145]
     ld hl, NoteNames
     call GetNthString
     push hl
     pop de
-    hlcoord 6+1, 16
+    hlcoord 5, 14
     call PlaceString
     ld a, [$c146]
     add $f6
-    hlcoord 6+3, 16
+    hlcoord 7, 14
     ld [hli], a
 
-; CHANNEL 3
-    printbit Channel3+3,  0, 12+0, 15, "1", "-" ; enabled
-    
+; CHANNEL 3    
     ld a, [$c177]
     ld hl, NoteNames
     call GetNthString
     push hl
     pop de
-    hlcoord 12+1, 16
+    hlcoord 10, 14
     call PlaceString
     ld a, [$c178]
     add $f6
-    hlcoord 12+3, 16
+    hlcoord 12, 14
     ld [hli], a
     	
 ; CHANNEL 4
-    printbit Channel4+3,  0, 18+0, 15, "1", "-" ; enabled
     ret
 
 DrawSongInfo:
     ld a, [wSongSelection]
     ld b, a
+	ld c, 0
     ld hl, SongInfo
 .loop
-    ld a, [hli]
+	ld a, [hl]
     cp -1
     jr z, .noname
+	inc c
+    ld a, c
     cp b
     jr z, .found
 .loop2
@@ -253,7 +242,7 @@ DrawSongInfo:
 	pop de
     inc de
 	push de
-	call GetSongArtist
+	call GetSongArtist2
     hlcoord 0, 11
     call PlaceString
 	pop de
@@ -300,6 +289,26 @@ GetSongArtist:
 	ld de, BlankName
     ret
 
+GetSongArtist2:
+    ld a, [de]
+    ld b, a
+    ld hl, Artist
+.loop
+    ld a, [hli]
+    cp -1
+    jr z, .noname
+    cp b
+    jr nz, .loop
+    push hl
+	ld de, Additional
+	hlcoord 0, 10
+    call PlaceString
+    pop de
+    ret
+.noname
+	ld de, BlankName
+    ret
+
 MusicPlayerText:
     db "--- MUSIC PLAYER ---@"
 
@@ -320,185 +329,189 @@ db "                    "
 db "Composer:           "
 db "                    "
 db "                    "
-db "Arranger:           "
 db "                    "
 db "                    "
-db "─Ch1───Ch2───Ch3─Ch4"
-db "     │     │     │  "
-db "     │     │     │  "
-db "     │     │     │  "
+db "                    "
+db "Ch1──Ch2──Wave─Noise"
+db "    │    │    │     "
+db "    │    │    │     "
+db "    │    │    │     "
 db "────────────────────"
 MPTilemapEnd
+
+Additional:
+	db "Additional Credits:@"
 
 BlankName:
 	db " @"
 SongInfo:
-    db 001, "Title Screen@", 3, 1, 1
-    db 002, "Route 1@", 3, 1, 1
-    db 003, "Route 3@", 3, 1, 1
-    db 004, "Route 11@", 3, 1, 1
-    db 005, "Magnet Train@", 3, 1, 1
-    db 006, "Vs. Kanto Gym Leader@", 3, 1, 1
-    db 007, "Vs. Kanto Trainer@", 3, 1, 1
-    db 008, "Vs. Kanto Wild@", 3, 1, 1
-    db 009, "Pokémon Center@", 3, 1, 1
-    db 010, "Spotted! Hiker@", 3, 1, 1
-    db 011, "Spotted! Girl 1@", 3, 1, 1
-    db 012, "Spotted! Boy 1@", 3, 1, 1
-    db 013, "Heal Pokémon@", 3, 1, 1
-    db 014, "Lavender Town@", 3, 1, 1
-    db 015, "Viridian Forest@", 3, 1, 1
-    db 016, "Kanto Cave@", 3, 1, 1
-    db 017, "Follow Me!@", 3, 1, 1
-    db 018, "Game Corner@", 3, 1, 1
-    db 019, "Bicycle@", 3, 1, 1
-    db 020, "Hall of Fame@", 3, 1, 1
-    db 021, "Viridian City@", 3, 1, 1
-    db 022, "Celedon City@", 3, 1, 1
-    db 023, "Victory! Trainer@", 3, 1, 1
-    db 024, "Victory! Wild@", 3, 1, 1
-    db 025, "Victory! Champion@", 3, 1, 1
-    db 026, "Mt. Moon@", 3, 1, 1
-    db 027, "Gym@", 3, 1, 1
-    db 028, "Pallet Town@", 3, 1, 1
-    db 029, "Oak's Lab@", 3, 1, 1
-    db 030, "Professor Oak@", 3, 1, 1
-    db 031, "Rival Appears@", 3, 1, 1
-    db 032, "Rival Departure@", 3, 1, 1
-    db 033, "Surfing@", 3, 1, 1
-    db 034, "Evolution@", 3, 1, 1
-    db 035, "National Park@", 3, 1, 1
-    db 036, "Credits@", 3, 1, 1
-    db 037, "Azalea Town@", 3, 1, 1
-    db 038, "Cherrygrove City@", 3, 1, 1
-    db 039, "Spotted! Kimono Girl@", 3, 1, 1
-    db 040, "Union Cave@", 3, 1, 1
-    db 041, "Vs. Johto Wild@", 3, 1, 1
-    db 042, "Vs. Johto Trainer@", 3, 1, 1
-    db 043, "Route 30@", 3, 1, 1
-    db 044, "Ecruteak City@", 3, 1, 1
-    db 045, "Violet City@", 3, 1, 1
-    db 046, "Vs. Johto Gym Leader@", 3, 1, 1
-    db 047, "Vs. Champion@", 3, 1, 1
-    db 048, "Vs. Rival@", 3, 1, 1
-    db 049, "Vs. Rocket Grunt@", 3, 1, 1
-    db 050, "Elm's Lab@", 3, 1, 1
-    db 051, "Dark Cave@", 3, 1, 1
-    db 052, "Route 29@", 3, 1, 1
-    db 053, "Route 34@", 3, 1, 1
-    db 054, "S.S. Aqua@", 3, 1, 1
-    db 055, "Spotted! Boy 2@", 3, 1, 1
-    db 056, "Spotted! Girl 2@", 3, 1, 1
-    db 057, "Spotted! Team Rocket@", 3, 1, 1
-    db 058, "Spotted! Suspicious@", 3, 1, 1
-    db 059, "Spotted! Sage@", 3, 1, 1
-    db 060, "New Bark Town@", 3, 1, 1
-    db 061, "Goldenrod City@", 3, 1, 1
-    db 062, "Vermilion City@", 3, 1, 1
-    db 063, "Pokémon Channel@", 3, 1, 1
-    db 064, "PokéFlute@", 3, 1, 1
-    db 065, "Tin Tower@", 3, 1, 1
-    db 066, "Sprout Tower@", 3, 1, 1
-    db 067, "Burned Tower@", 3, 1, 1
-    db 068, "Olivine Lighthouse@", 3, 1, 1
-    db 069, "Route 42@", 3, 1, 1
-    db 070, "Indigo Plateau@", 3, 1, 1
-    db 071, "Route 38@", 3, 1, 1
-    db 072, "Rocket Hideout@", 3, 1, 1
-    db 073, "Dragon's Den@", 3, 1, 1
-    db 074, "Vs. Johto Wild Night@", 3, 1, 1
-    db 075, "Unown Radio@", 3, 1, 1
-    db 076, "Captured Pokémon@", 3, 1, 1
-    db 077, "Route 26@", 3, 1, 1
-    db 078, "Mom@", 3, 1, 1
-    db 079, "Victory Road@", 3, 1, 1
-    db 080, "Pokémon Lullaby@", 3, 1, 1
-    db 081, "Pokémon March@", 3, 1, 1
-    db 082, "Opening 1@", 3, 1, 1
-    db 083, "Opening 2@", 3, 1, 1
-    db 084, "Load Game@", 3, 1, 1
-    db 085, "Ruins of Alph Inside@", 3, 1, 1
-    db 086, "Team Rocket@", 3, 1, 1
-    db 087, "Dancing Hall@", 3, 1, 1
-    db 088, "Bug Contest Ranking@", 3, 1, 1
-    db 089, "Bug Contest@", 3, 1, 1
-    db 090, "Rocket Radio@", 3, 1, 1
-    db 091, "GameBoy Printer@", 3, 1, 1
-    db 092, "Post Credits@", 3, 1, 1
-    db 093, "Clair@", 4, 1, 1
-    db 094, "Mobile Adapter Menu@", 4, 1, 1
-    db 095, "Mobile Adapter@", 4, 1, 1
-    db 096, "Buena's Password@", 4, 1, 1
-    db 097, "Eusine@", 4, 1, 1
-    db 098, "Opening@", 4, 1, 1
-    db 099, "Battle Tower@", 4, 1, 1
-    db 100, "Vs. Suicune@", 4, 1, 1
-    db 101, "Battle Tower Lobby@", 4, 1, 1
-    db 102, "Mobile Center@", 4, 1, 1
-    db 103, "Opening@", 1, 1, 1
-    db 104, "Opening@", 2, 1, 1
-    db 105, "Title Screen@", 1, 1, 1
-    db 106, "Introduction@", 1, 1, 1
-    db 107, "Pallet Town@", 1, 1, 1
-    db 108, "Professor Oak@", 1, 1, 1
-    db 109, "Oak's Lab@", 1, 1, 1
-    db 110, "Rival Appears@", 1, 1, 1
-    db 111, "Rival Departure@", 1, 1, 1
-    db 112, "Route 1@", 1, 1, 1
-    db 113, "Viridian City@", 1, 1, 1
-    db 114, "Pokémon Center@", 1, 1, 1
-    db 115, "Heal Pokémon@", 1, 1, 1
-    db 116, "Viridian Forest@", 1, 1, 1
-    db 117, "Follow Me!@", 1, 1, 1
-    db 118, "Gym@", 1, 1, 1
-    db 119, "Jigglypuff Sings@", 1, 1, 1
-    db 120, "Route 3@", 1, 1, 1
-    db 121, "Mt. Moon@", 1, 1, 1
-    db 122, "Jessie and James@", 2, 1, 1
-    db 123, "Cerulean City@", 1, 1, 1
-    db 124, "Vermilion City@", 1, 1, 1
-    db 125, "S.S. Anne@", 1, 1, 1
-    db 126, "Route 11@", 1, 1, 1
-    db 127, "Lavender Town@", 1, 1, 1
-    db 128, "Pokémon Tower@", 1, 1, 1
-    db 129, "Celedon City@", 1, 1, 1
-    db 130, "Game Corner@", 1, 1, 1
-    db 131, "Rocket Hideout@", 1, 1, 1
-    db 132, "Bicycle@", 1, 1, 1
-    db 133, "Evolution@", 1, 1, 1
-    db 134, "Surfing Pikachu@", 2, 1, 1
-    db 135, "Silph Co.@", 1, 1, 1
-    db 136, "Surfing@", 1, 1, 1
-    db 137, "Cinnabar Island@", 1, 1, 1
-    db 138, "Cinnabar Mansion@", 1, 1, 1
-    db 139, "Indigo Plateau@", 1, 1, 1
-    db 140, "Hall of Fame@", 1, 1, 1
-    db 141, "Credits@", 1, 1, 1
-    db 142, "Spotted! Boy@", 1, 1, 1
-    db 143, "Spotted! Girl@", 1, 1, 1
-    db 144, "Spotted! Rocket@", 1, 1, 1
-    db 145, "Vs. Wild@", 1, 1, 1
-    db 146, "Vs. Trainer@", 1, 1, 1
-    db 147, "Vs. Gym Leader@", 1, 1, 1
-    db 148, "Vs. Champion@", 1, 1, 1
-    db 149, "Victory! Wild@", 1, 1, 1
-    db 150, "Victory! Trainer@", 1, 1, 1
-    db 151, "Victory! Champion@", 1, 1, 1
-    db 152, "Unused@", 1, 1, 1
-    db 153, "Unused@", 2, 1, 1
-    db 154, "Vs. Wild@", 6, 1, 2
-    db 155, "Vs. Trainer@", 6, 1, 2
-    db 156, "Route 206@", 6, 1, 2
-    db 157, "PokéRadar@", 6, 1, 2
-    db 158, "Cerulean City@", 7, 1, 2
-    db 159, "Cinnabar Island@", 7, 1, 2
-    db 160, "Route 24@", 7, 1, 2
-    db 161, "Shop@", 7, 1, 2
-    db 162, "Pokéathelon Finals@", 7, 1, 2
-    db 163, "Vs. Johto Trainer   GS Kanto Style Remix@", 3, 1, 2
-    db 164, "Vs. Naljo Wild@", 11, 3, 2
-    db 165, "Vs. Naljo Gym Leader@", 11, 4, 2
-    db 166, "Vs. Pallet Patrol@", 11, 5, 2
+    db "Title Screen@", 3, 1, 0
+    db "Route 1@", 3, 1, 0
+    db "Route 3@", 3, 1, 0
+    db "Route 11@", 3, 1, 0
+    db "Magnet Train@", 3, 1, 0
+    db "Vs. Kanto Gym Leader@", 3, 1, 0
+    db "Vs. Kanto Trainer@", 3, 1, 0
+    db "Vs. Kanto Wild@", 3, 1, 0
+    db "Pokémon Center@", 3, 1, 0
+    db "Spotted! Hiker@", 3, 1, 0
+    db "Spotted! Girl 1@", 3, 1, 0
+    db "Spotted! Boy 1@", 3, 1, 0
+    db "Heal Pokémon@", 3, 1, 0
+    db "Lavender Town@", 3, 1, 0
+    db "Viridian Forest@", 3, 1, 0
+    db "Kanto Cave@", 3, 1, 0
+    db "Follow Me!@", 3, 1, 0
+    db "Game Corner@", 3, 1, 0
+    db "Bicycle@", 3, 1, 0
+    db "Hall of Fame@", 3, 1, 0
+    db "Viridian City@", 3, 1, 0
+    db "Celedon City@", 3, 1, 0
+    db "Victory! Trainer@", 3, 1, 0
+    db "Victory! Wild@", 3, 1, 0
+    db "Victory! Champion@", 3, 1, 0
+    db "Mt. Moon@", 3, 1, 0
+    db "Gym@", 3, 1, 0
+    db "Pallet Town@", 3, 1, 0
+    db "Oak's Lab@", 3, 1, 0
+    db "Professor Oak@", 3, 1, 0
+    db "Rival Appears@", 3, 1, 0
+    db "Rival Departure@", 3, 1, 0
+    db "Surfing@", 3, 1, 0
+    db "Evolution@", 3, 1, 0
+    db "National Park@", 3, 1, 0
+    db "Credits@", 3, 1, 0
+    db "Azalea Town@", 3, 1, 0
+    db "Cherrygrove City@", 3, 1, 0
+    db "Spotted! Kimono Girl@", 3, 1, 0
+    db "Union Cave@", 3, 1, 0
+    db "Vs. Johto Wild@", 3, 1, 0
+    db "Vs. Johto Trainer@", 3, 1, 0
+    db "Route 30@", 3, 1, 0
+    db "Ecruteak City@", 3, 1, 0
+    db "Violet City@", 3, 1, 0
+    db "Vs. Johto Gym Leader@", 3, 1, 0
+    db "Vs. Champion@", 3, 1, 0
+    db "Vs. Rival@", 3, 1, 0
+    db "Vs. Rocket Grunt@", 3, 1, 0
+    db "Elm's Lab@", 3, 1, 0
+    db "Dark Cave@", 3, 1, 0
+    db "Route 29@", 3, 1, 0
+    db "Route 34@", 3, 1, 0
+    db "S.S. Aqua@", 3, 1, 0
+    db "Spotted! Boy 2@", 3, 1, 0
+    db "Spotted! Girl 2@", 3, 1, 0
+    db "Spotted! Team Rocket@", 3, 1, 0
+    db "Spotted! Suspicious@", 3, 1, 0
+    db "Spotted! Sage@", 3, 1, 0
+    db "New Bark Town@", 3, 1, 0
+    db "Goldenrod City@", 3, 1, 0
+    db "Vermilion City@", 3, 1, 0
+    db "Pokémon Channel@", 3, 1, 0
+    db "PokéFlute@", 3, 1, 0
+    db "Tin Tower@", 3, 1, 0
+    db "Sprout Tower@", 3, 1, 0
+    db "Burned Tower@", 3, 1, 0
+    db "Olivine Lighthouse@", 3, 1, 0
+    db "Route 42@", 3, 1, 0
+    db "Indigo Plateau@", 3, 1, 0
+    db "Route 38@", 3, 1, 0
+    db "Rocket Hideout@", 3, 1, 0
+    db "Dragon's Den@", 3, 1, 0
+    db "Vs. Johto Wild Night@", 3, 1, 0
+    db "Unown Radio@", 3, 1, 0
+    db "Captured Pokémon@", 3, 1, 0
+    db "Route 26@", 3, 1, 0
+    db "Mom@", 3, 1, 0
+    db "Victory Road@", 3, 1, 0
+    db "Pokémon Lullaby@", 3, 1, 0
+    db "Pokémon March@", 3, 1, 0
+    db "Opening 1@", 3, 1, 0
+    db "Opening 2@", 3, 1, 0
+    db "Load Game@", 3, 1, 0
+    db "Ruins of Alph Inside@", 3, 1, 0
+    db "Team Rocket@", 3, 1, 0
+    db "Dancing Hall@", 3, 1, 0
+    db "Bug Contest Ranking@", 3, 1, 0
+    db "Bug Contest@", 3, 1, 0
+    db "Rocket Radio@", 3, 1, 0
+    db "GameBoy Printer@", 3, 1, 0
+    db "Post Credits@", 3, 1, 0
+    db "Clair@", 4, 1, 0
+    db "Mobile Adapter Menu@", 4, 1, 0
+    db "Mobile Adapter@", 4, 1, 0
+    db "Buena's Password@", 4, 1, 0
+    db "Eusine@", 4, 1, 0
+    db "Opening@", 4, 1, 0
+    db "Battle Tower@", 4, 1, 0
+    db "Vs. Suicune@", 4, 1, 0
+    db "Battle Tower Lobby@", 4, 1, 0
+    db "Mobile Center@", 4, 1, 0
+    db "Opening@",  1, 1, 0
+    db "Opening@", 2, 1, 0
+    db "Title Screen@",  1, 1, 0
+    db "Introduction@",  1, 1, 0
+    db "Pallet Town@",  1, 1, 0
+    db "Professor Oak@",  1, 1, 0
+    db "Oak's Lab@",  1, 1, 0
+    db "Rival Appears@",  1, 1, 0
+    db "Rival Departure@",  1, 1, 0
+    db "Route 1@",  1, 1, 0
+    db "Viridian City@",  1, 1, 0
+    db "Pokémon Center@",  1, 1, 0
+    db "Heal Pokémon@",  1, 1, 0
+    db "Viridian Forest@",  1, 1, 0
+    db "Follow Me!@",  1, 1, 0
+    db "Gym@",  1, 1, 0
+    db "Jigglypuff Sings@",  1, 1, 0
+    db "Route 3@",  1, 1, 0
+    db "Mt. Moon@",  1, 1, 0
+    db "Jessie and James@", 2, 1, 0
+    db "Cerulean City@",  1, 1, 0
+    db "Vermilion City@",  1, 1, 0
+    db "S.S. Anne@",  1, 1, 0
+    db "Route 11@",  1, 1, 0
+    db "Lavender Town@",  1, 1, 0
+    db "Pokémon Tower@",  1, 1, 0
+    db "Celedon City@",  1, 1, 0
+    db "Game Corner@",  1, 1, 0
+    db "Rocket Hideout@",  1, 1, 0
+    db "Bicycle@",  1, 1, 0
+    db "Evolution@",  1, 1, 0
+    db "Surfing Pikachu@", 2, 1, 0
+    db "Silph Co.@",  1, 1, 0
+    db "Surfing@",  1, 1, 0
+    db "Cinnabar Island@",  1, 1, 0
+    db "Cinnabar Mansion@",  1, 1, 0
+    db "Indigo Plateau@",  1, 1, 0
+    db "Hall of Fame@",  1, 1, 0
+    db "Credits@",  1, 1, 0
+    db "Spotted! Boy@",  1, 1, 0
+    db "Spotted! Girl@",  1, 1, 0
+    db "Spotted! Rocket@",  1, 1, 0
+    db "Vs. Wild@",  1, 1, 0
+    db "Vs. Trainer@",  1, 1, 0
+    db "Vs. Gym Leader@",  1, 1, 0
+    db "Vs. Champion@",  1, 1, 0
+    db "Victory! Wild@",  1, 1, 0
+    db "Victory! Trainer@",  1, 1, 0
+    db "Victory! Champion@",  1, 1, 0
+    db "Unused@",  1, 1, 0
+    db "Unused@", 2, 1, 0
+    db "Vs. Wild@", 6, 1, 2
+    db "Vs. Trainer@", 6, 1, 2
+    db "Jubilife City@", 6, 1, 2
+    db "Route 206@", 6, 1, 2
+    db "PokéRadar@", 6, 1, 2
+    db "Cerulean City@", 7, 1, 2
+    db "Cinnabar Island@", 7, 1, 2
+    db "Route 24@", 7, 1, 2
+    db "Shop@", 7, 1, 2
+    db "Pokéathelon Finals@", 7, 1, 2
+    db "Vs. Johto Trainer   GS Kanto Style Remix@", 3, 1, 2
+    db "Vs. Naljo Wild@", 11, 3, 2
+    db "Vs. Naljo Gym Leader@", 11, 4, 2
+    db "Vs. Pallet Patrol@", 11, 5, 2
     db -1
 	
 Origin:
